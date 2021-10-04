77909b614f2bb09d61ba53eaefcef4e8570682b3
MuresanIA <muresan.alexandru933@gmail.com>
2021-10-04 09:58:53 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CreditCardDirective } from './credit-card/credit-card.directive';
+
+@NgModule({
+  declarations: [AppComponent, CreditCardDirective],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
